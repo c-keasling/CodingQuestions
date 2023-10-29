25fa4368f64185ac9a576d004a86719507d5073d
c-keasling <132222567+c-keasling@users.noreply.github.com>
2023-10-28 20:10:12 -0700

    Update 2703. Return Length of ArgumentsPassed.js
    added comments

--- LeetCode/2703. Return Length of ArgumentsPassed.js
+++ LeetCode/2703. Return Length of ArgumentsPassed.js
@@ -4,6 +4,7 @@
  * @return {number}
  */
 var argumentsLength = function (...args) {
+  //learned the .lenghts function and used it to return the lenghts of all the arguments passed through. I think this is useful when learning about what can be done with passing variables through functions with spread operators
   return args.length;
 };
 
