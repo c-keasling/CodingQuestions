cfb433054b95cebc4b6d51d428611a38ff369daa
c-keasling <132222567+c-keasling@users.noreply.github.com>
2023-10-28 19:59:42 -0700

    Update 2620. Counter.js

--- LeetCode/2620. Counter.js
+++ LeetCode/2620. Counter.js
@@ -4,10 +4,18 @@
  * @return {Function} counter
  */
 var createCounter = function (n) {
+  // Store initial number inside createCounter to represents the starting number of the counter.
   let initialNum = n;
+  
+  // This inner function has access to 'initialNum' from the outer function's scope.
   return function () {
+    // Declare a local variable returnNum in the inner function scope so that this inner function can return a number. On the first call it will equal initial number
     let returnNum = initialNum;
+    
+    // add one to initialNum to increase it for the next call.
     initialNum++;
+    
+    // The inner function returns the returnNum so that it matches the times it was called
     return returnNum;
   };
 };
